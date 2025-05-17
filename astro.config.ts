cfad163447068f53a96614493cf4718d4550f07e
ryangst <ryangst.hire@gmail.com>
2025-05-17 18:33:32 -0300

    Update Astro configuration to use Node adapter, upgrade dependencies, and adjust package versions

--- astro.config.ts
+++ astro.config.ts
@@ -8,9 +8,12 @@ import { SITE } from "./src/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -18,6 +21,7 @@ export default defineConfig({
     react(),
     sitemap(),
   ],
+
   markdown: {
     remarkPlugins: [
       remarkToc,
@@ -33,12 +37,17 @@ export default defineConfig({
       wrap: true,
     },
   },
+
   vite: {
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
   },
+
   scopedStyleStrategy: "where",
   output: "server",
-  adapter: vercel({}),
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
